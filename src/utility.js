ba8adf058238dd0a2931d9fe5c03391606f39af3
Alessandro Travi <alessandro.travi@xpeppers.com>
2020-02-20 17:18:37 +0100

    [feat]: added code

--- src/utility.js
+++ src/utility.js
@@ -1,13 +1,7 @@
-class Utility {
+module.exports.replaceAll = function (str, mapObj) {
+    const re = new RegExp(Object.keys(mapObj).join("|"),"gi");
 
-    replaceAll(str,mapObj){
-        const re = new RegExp(Object.keys(mapObj).join("|"),"gi");
-
-        return str.replace(re, function(matched){
-            return mapObj[matched.toLowerCase()];
-        });
-    };
-
-}
-
-module.exports = Utility;
+    return str.replace(re, function(matched){
+        return mapObj[matched.toLowerCase()];
+    });
+};
