16cf6da5dfbb8d7ec7d0d1b2bb05611fd7fb96e6
Alessandro Travi <alessandro.travi@xpeppers.com>
2020-02-20 14:52:57 +0100

    [feat]: added code

---
+++ index.js
@@ -0,0 +1,34 @@
+import {Telegram} from "telegraf";
+
+const Extra = require('telegraf/extra');
+const Markup = require('telegraf/markup');
+const express = require('express');
+const expressApp = express();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const telegram = new Telegram(process.env.TELEGRAM_TOKEN);
+
+const keyboard = Markup.inlineKeyboard([
+    Markup.urlButton('❤️', 'http://telegraf.js.org'),
+    Markup.callbackButton('Delete', 'delete')
+]);
+
+expressApp.get('/health', (req, res) => {
+    res.send('Status: OK')
+});
+
+expressApp.post('/netlify-hook', jsonParser, (req, res) => {
+    console.log(req.body);
+
+    telegram.sendCopy(process.env.CHANNEL_ID, ctx.message, Extra.markup(keyboard));
+    res.status(200)
+});
+
+expressApp.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+});
+
+
+
