460f5614e3bd694ece74f6d658040bee4f4429ac
Alessandro Travi <alessandro.travi@xpeppers.com>
2020-02-20 17:43:49 +0100

    [fix]: test new messages

--- src/server.js
+++ src/server.js
@@ -17,26 +17,38 @@ expressApp.get('/health', (req, res) => {
 });
 
 expressApp.post('/netlify-hook/deploy/succeeded', jsonParser, (req, res) => {
-    console.log(req.body);
     res.json({status: "ok"});
 
     const mapObj = {
         "{{site}}": req.body.url,
+        "{{user}}": req.body.committer,
     };
 
     telegram.sendMessage(process.env.CHANNEL_ID, Utility.replaceAll(config.deploySucceededMessage, mapObj), Extra.markdown());
 });
 
 expressApp.post('/netlify-hook/deploy/failed', jsonParser, (req, res) => {
-    console.log(req.body);
     res.json({status: "ok"});
-    telegram.sendMessage(process.env.CHANNEL_ID, "Deploy failed");
+
+    const mapObj = {
+        "{{site}}": req.body.url,
+        "{{message}}": req.body.error_message,
+        "{{user}}": req.body.committer,
+    };
+
+    telegram.sendMessage(process.env.CHANNEL_ID, Utility.replaceAll(config.deployFailedMessage, mapObj), Extra.markdown());
 });
 
 expressApp.post('/netlify-hook/form', jsonParser, (req, res) => {
-    console.log(req.body);
     res.json({status: "ok"});
-    telegram.sendMessage(process.env.CHANNEL_ID, "test");
+
+    const mapObj = {
+        "{{site}}": req.body.site_url,
+        "{{sender_name}}": req.body.data.name,
+        "{{message}}": req.body.data.message,
+    };
+
+    telegram.sendMessage(process.env.CHANNEL_ID, Utility.replaceAll(config.deployFailedMessage, mapObj), Extra.markdown());
 });
 
 expressApp.listen(port, () => {
