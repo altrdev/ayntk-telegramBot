0d80745a7f0fda5d7f3d32c23afc43d373e23cdf
Alessandro Travi <alessandro.travi@xpeppers.com>
2020-02-20 17:00:03 +0100

    [feat]: added code

--- src/utility.js
+++ src/utility.js
@@ -1,10 +1,16 @@
-function replaceAll(str,mapObj){
+const deploySucceededMessage = () => {
+    return "Deploy succeeded for {{site}}";
+};
+
+const replaceAll = (str,mapObj) => {
     const re = new RegExp(Object.keys(mapObj).join("|"),"gi");
 
     return str.replace(re, function(matched){
         return mapObj[matched.toLowerCase()];
     });
-}
+};
 
-
-const deploySucceededMessage = "Deploy succeeded for {{site}}";
+module.exports = {
+    replaceAll: replaceAll,
+    deploySucceededMessage: deploySucceededMessage
+};
